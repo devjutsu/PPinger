08ca5fa4d879242318bf02cf42090a2eed40d8cf
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-02 18:34:11 +0300

    smallfix

--- ppinger.py
+++ ppinger.py
@@ -1,14 +1,14 @@
 import os
 import time
-hostname = "192.168.1.109"
-response = os.system("ping -c 1 " + hostname)
-
+hostname = "192.168.1.109" #example
 
 while(True):
   f = open("/home/garuda/results.txt", "a")
+  response = os.system("ping -c 1 " + hostname)
+
   if response == 0:
     f.write(time.ctime() + ' ___ UP!\n')
   else:
     f.write(time.ctime() + ' ___ \n')
   f.close()
-  time.sleep(60);
+  time.sleep(300);
